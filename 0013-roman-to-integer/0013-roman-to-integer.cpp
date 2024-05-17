019fb5662a196e02353a00b64b3a12e7a5f3c9c3
Unknown-Blaze <109670784+Unknown-Blaze@users.noreply.github.com>
2024-05-17 21:39:32 +0800

    Time: 19 ms (12.19%), Space: 12.7 MB (39.87%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> map;
+        map['I'] = 1;
+        map['V'] = 5;
+        map['X'] = 10;
+        map['L'] = 50;
+        map['C'] = 100;
+        map['D'] = 500;
+        map['M'] = 1000;
+        int ret = 0;
+        for (int i = 0; i < s.size(); i++){
+            if (i < s.size() - 1 && map[s[i]] < map[s[i+1]]) {
+                ret -= map[s[i]];
+            } else {
+                ret += map[s[i]];
+            }
+
+        }
+        return ret;
+    }
+};
