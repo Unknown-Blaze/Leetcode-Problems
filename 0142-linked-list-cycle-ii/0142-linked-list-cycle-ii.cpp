a683f8358f35edf23c430ea72f5bbd9b62a7ca21
Unknown-Blaze <109670784+Unknown-Blaze@users.noreply.github.com>
2024-05-17 22:19:29 +0800

    Time: 7 ms (54.39%), Space: 13.1 MB (7.87%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* ptr = head;
+        unordered_set <ListNode*> s;
+
+        while (ptr != NULL){
+
+            if (s.find(ptr) != s.end()){
+                return ptr;
+            }
+            s.insert(ptr);
+            ptr = ptr->next;
+        }
+        return NULL;
+    }
+};
