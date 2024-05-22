ea5f67ea053adb9434a059352a66e81959d938d0
Unknown-Blaze <109670784+Unknown-Blaze@users.noreply.github.com>
2024-05-22 16:21:29 +0800

    Time: 0 ms (100.00%), Space: 14.1 MB (20.20%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> map1;
+        unordered_map<int, int> map;
         vector<int> ret;
-        for (int i = 0; i < nums1.size(); i++){
-            map1[nums1[i]]++;
-        }
         for (int i = 0; i < nums2.size(); i++){
-            if (map1[nums2[i]]) {
-                map1[nums2[i]]--;
-                ret.push_back(nums2[i]);
+            map[nums2[i]]++;
+        }
+        for (int i = 0; i < nums1.size(); i++){
+            if (map[nums1[i]]) {
+                map[nums1[i]]--;
+                ret.push_back(nums1[i]);
             }
         }
         return ret;
