0a2af2088afa414e3f36d6c940bfe01516b87f78
Unknown-Blaze <109670784+Unknown-Blaze@users.noreply.github.com>
2024-05-28 00:02:12 +0800

    Time: 0 ms (100.00%), Space: 10 MB (85.84%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int specialArray(vector<int>& nums) {
+        
+        int count;
+        for (int i = 0; i <= nums.size(); i++){
+            count = 0;
+            for (int j = 0; j < nums.size(); j++){
+                if (i <= nums[j]) count++;
+            }
+            if (count == i) return i;
+        }
+        return -1;
+    }
+};
